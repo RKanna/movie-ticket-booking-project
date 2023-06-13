3aa130e08e64d9e454510bbb88ecff320de602b9
RKanna <rkannan.balakrishnan@hotmail.com>
2023-06-13 20:16:46 +0530

    initial script

---
+++ scripts/script.js
@@ -0,0 +1,20 @@
+"use strict";
+
+//getting and assigning input and output variables
+//btns
+
+//container
+
+//overlay
+
+//for selecting all buttons at once
+
+//creating function
+const init = function () {
+  modelContainer.classList.add("hidden");
+};
+init();
+
+//adding function for close and open
+
+//loop for querySelectorAll
